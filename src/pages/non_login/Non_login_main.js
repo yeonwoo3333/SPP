361061259d7ada57744f4258fd6ee3ae4b02e583
seunghoo090004 <ssm090004@gmail.com>
2024-05-26 14:19:56 +0900

    Non_login_0526

--- src/pages/non_login/Non_login_main.js
+++ src/pages/non_login/Non_login_main.js
@@ -1,28 +1,51 @@
-import React from "react";
-import { Link, Route } from 'react-router-dom';
-import "../Main.css";
-
-import "./Non_login.css"
+import React from 'react';
 import NavBar from '../component/NavBar';
+import '../Main.css';
+import './Non_login.css';
 
 // 화면 이미지
-import IntroductionImage from "../Image/main_page.jpg" 
+import IntroductionImage from '../Image/main_page.jpg';
 
 const Main = () => {
-return (
+  return (
     <div>
-        <div>
-            <NavBar />
-        </div>
-
-
-    <div className="image-container">
-        {/* 소개 화면 이미지 */}
+      <NavBar />
+      
+      <div className="image-container">
         <img src={IntroductionImage} alt="대표 이미지" />
+      </div>
+      
+      <div className="main-content">
+        <section className="section-intro">
+          <h2>서비스 소개</h2>
+          <p>플래너 서비스 설명 ~~~.</p>
+        </section>
+        
+        <section className="service-list">
+          <h2>제공 서비스</h2>
+          <ul>
+            <li>일정 관리:</li>
+            <li>보고서 작성:</li>
+            <li>계약 관리:</li>
+          </ul>
+        </section>
+        
+        <section className="testimonials">
+          <h2>고객 후기</h2>
+          <p>"플래너 덕분에 좋았다~!" - 이모씨</p>
+        </section>
+        
+        <section className="additional-info">
+          <h2>추가 정보</h2>
+          <p>다양한 것들~</p>
+        </section>
+        
+        <footer>
+          <p>문의 사항은 연우@planner.com으로 연락 주세요.</p>
+        </footer>
+      </div>
     </div>
-
-    </div>
-);
+  );
 };
 
 export default Main;
